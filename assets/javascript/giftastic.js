2a2df5d7b07e80566b312bcf92b55d70aeaec1e7
joraro <engjoserobles@gmail.com>
2019-07-14 14:09:08 -0400

    Changed logic for swapping gifs.

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,9 +1,6 @@
 // Initial array of movies
 var movies = ["Computers", "Baseball", "Mathematics", "Automobiles"];
-//Create the array for the still images
-var stillImg = [];
-//Create the array for the animated images
-var animatedImg = [];
+
 
 // displayMovieInfo function re-renders the HTML to display the appropriate content
 function displayMovieInfo() {
@@ -11,8 +8,9 @@ function displayMovieInfo() {
   var searchString = $(this).attr("data-name");
   // var queryURL  = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
   var queryURL     =  "https://api.giphy.com/v1/gifs/search?api_key=Oro0OZBXIqGCkXF4o6kQwSkMwlOjwRAe&q=" 
-  + searchString + 
-                      "&limit=10&offset=0&rating=G&lang=en";
+  + searchString + "&limit=10&offset=0&rating=G&lang=en";
+  
+  $("#gifs-view").empty()
 
   // Creating an AJAX call for the specific GIF button being clicked
   $.ajax({
@@ -21,97 +19,39 @@ function displayMovieInfo() {
   }).then(function(response) {
 
     // Creating a div to hold the movie
-    var movieDiv = $("<div class='movie'>");
+    var gifDiv = $("<div class='gif-div'>");
 
-    // // Storing the rating data
-    // var rating = response.Rated;
 
-    // // Creating an element to have the rating displayed
-    // var pOne = $("<p>").text("Rating: " + rating);
-
-    // // Displaying the rating
-    // movieDiv.append(pOne);
-
-    // // Storing the release year
-    // var released = response.Released;
-
-    // // Creating an element to hold the release year
-    // var pTwo = $("<p>").text("Released: " + released);
-
-    // // Displaying the release year
-    // movieDiv.append(pTwo);
-
-    // // Storing the plot
-    // var plot = response.Plot;
-
-    // // Creating an element to hold the plot
-    // var pThree = $("<p>").text("Plot: " + plot);
-
-    // // Appending the plot
-    // movieDiv.append(pThree);
-
-    //Empty <div> before appending new images
-    movieDiv.empty();
-    //Empty the array for the still images
-    stillImg.length = 0;
-    //Empty the array for the animated images
-    animatedImg.length = 0;
     response.data.forEach (function (gifObject, gifIndex) { 
 
-      //create a new Image() object and push it into stillImg[]
-      //, setting its src attribute to the still image.
-      stillImg[gifIndex] = new Image();
+      gifImage = $("<img>"); 
       //add data attribute indicating if image is static or animated
-      $(stillImg[gifIndex]).attr("data-static","true");
-      $(stillImg[gifIndex]).attr("data-index", gifIndex);
-      stillImg[gifIndex].src = gifObject.images.fixed_height_still.url;
-      //create a new Image() object and push it into animatedImg[], 
-      //setting its src attribute to the animated image.
-      animatedImg[gifIndex] = new Image();
-      //add data attribute indicating if image is static or animated
-      $(animatedImg[gifIndex]).attr("data-static","false");
-      $(animatedImg[gifIndex]).attr("data-index", gifIndex); 
-      animatedImg[gifIndex].src = gifObject.images.fixed_height.url;
-      // Appending the image
-      movieDiv.append( $(stillImg[gifIndex]) );
+      gifImage.attr("src",          gifObject.images.fixed_height_still.url);
+      gifImage.attr("data-static",  gifObject.images.fixed_height_still.url);
+      gifImage.attr("data-animate", gifObject.images.fixed_height.url);
+      gifImage.attr("data-index",   gifIndex);
+      gifImage.attr("data-animated", "false");  
+      gifDiv.append(gifImage);
+      $("#gifs-view").append(gifDiv);
     });
-         
-    $("#movies-view").append(movieDiv);
-
   });
-
-  
-
 }
 
 
 function swapGifs()  {
 
-  // console.log("Clicked on :" + $(this).attr("src")); 
-  // console.log("Is it static? :" + $(this).attr("data-static")); 
-  // console.log ("Index of img is :" + $(this).attr("data-index") ); 
-  
+  var isAnimated = $(this).attr("data-animated"); 
 
-  //Get the image's position in the div. 
-  var imageIndex =  $(this).attr("data-index");
-  // console.log(animatedImg[imageIndex].src);
-  var tempImg = new Image();
-  
+    if (isAnimated === "true") { 
+      $(this).attr("src", $(this).attr("data-static"));
+      $(this).attr("data-animated", "false"); 
+    } 
+    else if(isAnimated === "false") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-animated", "true"); 
+    }
 
-  //If this is the static image, replace it with the animated
-  if ( $(this).attr("data-static")  === "true" ) {
-    $(tempImg).attr("src", $(this).attr("src"));
-    $(this).attr("src", animatedImg[imageIndex].src);
-    animatedImg[imageIndex].src = $(tempImg).attr("src"); 
-    $(this).attr("data-static", "false"); 
   }
-  else {
-    $(tempImg).attr("src", $(this).attr("src"));   
-    $(this).attr("src", animatedImg[imageIndex].src);
-    animatedImg[imageIndex].src = $(tempImg).attr("src"); 
-    $(this).attr("data-static", "true"); 
-  }
-};
 
 // Function for displaying movie data
 function renderButtons() {
